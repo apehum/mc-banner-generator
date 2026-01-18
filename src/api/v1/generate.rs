e6f6a3d39911fa72870122413adbc0e20f5a6603
Apehum <apehum@proton.me>
2026-01-19 01:23:39 +0800

    add width query param for /generate endpoint

--- src/api/v1/generate.rs
+++ src/api/v1/generate.rs
@@ -1,17 +1,25 @@
 use crate::api::error::AppError;
 use crate::api::state::AppState;
 use crate::banner::{apply_pattern, Banner, Pattern};
-use axum::extract::{Path, State};
+use axum::extract::{Path, Query, State};
 use axum::http::header;
 use axum::response::Response;
 use image::RgbaImage;
+use image::imageops::resize;
+use serde::Deserialize;
 use std::io::Cursor;
 
 const CACHE_TIME: i32 = 30 * 24 * 60 * 60;
 
+#[derive(Deserialize, Debug)]
+pub struct GenerateQuery {
+    pub width: Option<usize>,
+}
+
 pub async fn generate(
     State(app_state): State<AppState>,
     Path(banner_string): Path<String>,
+    Query(params): Query<GenerateQuery>,
 ) -> Result<Response, AppError> {
     let banner_without_extension = banner_string
         .chars()
@@ -31,6 +39,14 @@ pub async fn generate(
         let pattern_image = &app_state.patterns[pattern.pattern];
 
         apply_pattern(&mut image, pattern_image, pattern.color.to_rgb());
+    }
+
+    let image_width = params.width
+        .unwrap_or(20)
+        .clamp(20, 320) as u32;
+
+    if image_width != 20 {
+        image = resize(&image, image_width, image_width * 2, image::imageops::FilterType::Nearest);
     }
 
     let mut buf = Vec::new();
